79020c0ef30d9f6b711deb86750e954af6030c7b
ken <flying861110@foxmail.com>
2018-08-01 10:43:12 +0800

    Arrays.asList

---
+++ src/com/wey/concurrentSkipListSet/ConcurrentSkipListSetDemo.java
@@ -0,0 +1,57 @@
+package com.wey.concurrentSkipListSet;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.concurrent.ConcurrentSkipListSet;
+
+public class ConcurrentSkipListSetDemo {
+    
+    private static Set<String> set = new ConcurrentSkipListSet<String>();
+    
+    public static void main(String[] args) {
+        new MyThread("a").start();
+        new MyThread("b").start();
+        
+        Set<String> set1 = new HashSet<String>();
+        set1.add("apple");
+        set1.add("apple");
+        set1.add("orange");
+        System.out.println(set1);
+        
+        Set<String> set2 = new TreeSet<String>();
+        
+        set2.add("orange");
+        set2.add("orange");
+        set2.add("apple");
+        System.out.println(set2);
+    }
+    
+    private static void printAll() {
+        String value = null;
+        Iterator iter = set.iterator();
+        while (iter.hasNext()) {
+            value = (String) iter.next();
+            System.out.print(value + ", ");
+        }
+        System.out.println();
+    }
+    
+    private static class MyThread extends Thread {
+        
+        MyThread(String name) {
+            super(name);
+        }
+        
+        @Override
+        public void run() {
+            for (int i = 1; i < 10; i++) {
+                String value = Thread.currentThread().getName() + (i % 5);
+                set.add(value);
+                printAll();
+            }
+        }
+        
+    }
+}
