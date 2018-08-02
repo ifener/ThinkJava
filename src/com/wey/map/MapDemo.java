29f7f2963517fd2d9bc292ab2c8ff46acd732b95
ken <flying861110@foxmail.com>
2018-08-02 17:09:48 +0800

    map default capacity

--- src/com/wey/map/MapDemo.java
+++ src/com/wey/map/MapDemo.java
@@ -5,34 +5,33 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import com.wey.cloneable.Apple;
 import com.wey.cloneable.Fruit;
 import com.wey.cloneable.Orange;
 import com.wey.cloneable.Pear;
 
 public class MapDemo {
-	
-	public static Map<String, List<? extends Fruit>> maps = new HashMap<String, List<? extends Fruit>>();
-	static {
-		maps.put("a", Arrays.asList(new Orange(),new Pear()));
-		maps.put("b", Arrays.asList(new Orange(),new Pear(),new Pear()));
-	}
-	
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		System.out.println(maps.keySet());
-		for(String key:maps.keySet())
-		{
-			for(Fruit f:maps.get(key))
-			{
-				if(f instanceof Orange){
-					System.out.println(f);
-				}
-			}
-		}
-
-	}
-
+    
+    // 实例化MAP时，默认大小给16
+    public static Map<String, List<? extends Fruit>> maps = new HashMap<String, List<? extends Fruit>>(16);
+    static {
+        maps.put("a", Arrays.asList(new Orange(), new Pear()));
+        maps.put("b", Arrays.asList(new Orange(), new Pear(), new Pear()));
+        
+    }
+    
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println(maps.keySet());
+        for (String key : maps.keySet()) {
+            for (Fruit f : maps.get(key)) {
+                if (f instanceof Orange) {
+                    System.out.println(f);
+                }
+            }
+        }
+        
+    }
+    
 }
